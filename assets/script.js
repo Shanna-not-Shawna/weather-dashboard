186676e3253f5b669aabca7a188cdee82e3b2685
Shanna-not-Shawna <144177634+Shanna-not-Shawna@users.noreply.github.com>
2023-10-02 18:31:58 -0500

    linked to jQuery and bootstrap in html, added containers and form. Added styles to containers for working clarity. Declared basic variables and made contact with API with starter fetch.

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,11 @@
-var apikey = "b539fdcf084f125f2fbdb2489d4ebf94";
+var APIkey = "b539fdcf084f125f2fbdb2489d4ebf94";
+var searchInputVal = "Dallas";
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchInputVal + "&appid=" + APIkey + "&units=imperial";
+var currentDay = dayjs().format('MM/DD/YYYY');
 
-var city;
-apikey.openweathermap.org/data/2.5/weather?q=fortworth&appid=apikey
 
+fetch(queryURL)
+  .then(response => response.json())
+  .then(data => console.log(data));
+
+  // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
